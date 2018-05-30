c6e8021aa37537bbfe1ee29083547ba7dfae4eb4
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-05-30 10:12:12 -0500

    generating profile page html

---
+++ src/all_profiles.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function getProfiles() {
+    return fetch('http://localhost:3000/women')
+        .then(response => response.json())
+}
+
+
+function generateProfiles() {
+    let html = "";
+    getProfiles()
+        .then((profiles) => {
+            profiles.forEach((profile) => {
+                html +=
+                    `<div class ="card">
+                        <div class="card-header">${profile.name}</div>
+                        <div class="card-body">
+                            <h5 class="card-title">${profile.birthDate}-${profile.deathDate}</h5>
+                            <p class = "card-text">${profile.bio}</p>
+                            
+                        </div>`
+        });
+      $('#mainBody').html(html)
+}
+
