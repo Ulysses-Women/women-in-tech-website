c376a574d0991228affa78eeaee58f5db86849e6
monicacholico <monicachol@hotmail.com>
2018-06-05 23:28:57 -0500

    features: added images to the array

--- js/quotes.js
+++ js/quotes.js
@@ -9,39 +9,47 @@ var quotes = [
         text: "I am much pleased to find how very well I stand work & how my powers" +
         " of attention & continued effort increase.",
         author: "Adele Lovelace",
-        color: "black",
-        logoLocation: "img/Shirley-Ann.png"
+        color: "red",
+        image: "img/Karen_Sparck.png"
     },
     {
         text: "There is not just one Trans Movement. We have to learn to be allies to each other.",
         author: "Angelica Ross",
-        color: "navy"
+        color: "navy",
+        image: "img/Angelica-Ross.png"
     },
     {
         text: "I do know the technique, I don't apply it but my ideas. I get committed to it.",
         author: "Ida Holz",
-        color: "red"
+        color: "cadetblue",
+        image: "img/IdaHolz.png"
     },
     {
         text: "Engineers should strive to create a system that is designed for humans in their natural form.",
         author: "Radia Perlman",
-        color: "slateblue"
+        color: "slateblue",
+        image: "img/HollyGibson.png"
+
     },
     {
         text: "Social media is that daily feed that reminds people that you exist and it has to be as transparent" +
         " and true as everything else you do, or people will call you out on it.",
         author: "Frances Allen",
-        color: "orange"
+        color: "orange",
+        image: "img/Shirley-Ann-effect.png"
     },
     {
         text: "It's not subtle or restrained. It's not any of the things you like to think apply to your acting.",
         author: "Margaret Hamilton",
-        color: "seagreen"
+        color: "seagreen",
+        image: "img/AdeleGoldberg.png"
     },
     {
         text: "The first factor is self-motivation and passion to learn.",
         author: "Lesley Carhart",
-        color: "hotpink"
+        color: "hotpink",
+        image: "img/Lesley-Carhart.png"
+
     }
 ];
 
@@ -61,9 +69,11 @@ function displayQuote(quotes){
 
     var textElement = document.getElementById("text");
     var authorElement = document.getElementById("author");
+    var imageElement = document.getElementById("image");
 
     textElement.innerHTML = quote.text;
     authorElement.innerHTML = quote.author;
+    imageElement.innerHTML =`<img src=\"` + quote.image + `"/>`;
 
     var bigBubble = document.getElementById("bigBubble");
     bigBubble.style.background = quote.color;
